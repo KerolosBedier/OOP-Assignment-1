92bd46ae2d1943c44783e2c76138303434bcbce7
Kerolos Bedier <kerolos.bedier@gmail.com>
2023-10-13 15:20:51 +0300

    Frsit Commiation (Adding The Needed Files)

---
+++ CS213-2023-20220252-20220051-20220024-A1-Part1.cpp
@@ -0,0 +1,26 @@
+// FCAI – OOP Programming – 2023 - Assignment 1
+// Program Name:                CS213-2023-20220252-20220051-20220024-A1-Part1.cpp
+// Last Modification Date:      10/10/2023
+// Author1 and ID and Group:    Kerolos Bedier Soliman - 20220252 - N/A
+// Author2 and ID and Group:    Adham salah ezzat  - 20220051 - N/A
+// Author3 and ID and Group:    Ahmed Atef Mohamed - 20220024 - N/A
+// Teaching Assistant:          N/A
+// Purpose:                     Assignment 1 Image Processor
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include "bmplib.cpp"
+
+using namespace std;
+unsigned char image[SIZE][SIZE];
+
+
+int main() {
+
+	
+    return 0;
+}
+
+
